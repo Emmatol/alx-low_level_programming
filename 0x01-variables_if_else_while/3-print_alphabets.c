3b06648604963ef7b29a2695d91d14ca36435175
Emmatol <emmanueltoluwania@outlook.com>
2022-12-13 02:54:28 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,19 +2,18 @@
 
 /**
  * main - prints the alphabet in lowercase, and then in uppercase
- * followed by a new line, except m and q
+ * followed by a new line
  * Return: Always 0
  */
-
 int main(void)
 {
-	char la;
+	char ch;
 
-	for (la = 'a'; la <= 'z'; la++)
-	{
-		if (la != 'm' && la != 'q')
-			putchar(la);
-	}
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
 	putchar('\n');
 	return (0);
 }
